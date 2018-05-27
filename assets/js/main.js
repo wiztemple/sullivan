43e5cfb31cfce93f26c558efe89f3157c737a8d5
wiztemple <wiztemple7@gmail.com>
2018-05-27 06:35:37 +0100

    dashboard fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const footer = document.getElementById('footer')
 window.onscroll = function() {
-    if(window.pageYOffset >= 500){
+    if(window.pageYOffset >= 50){
         footer.style.display = 'block';
     } else {
       footer.style.display = 'none';
